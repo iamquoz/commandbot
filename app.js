b5314eb60e40901d5315ee6f8f4ed7d3cf055ef0
iamquoz <46261678+iamquoz@users.noreply.github.com>
2020-11-24 20:31:45 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 require('dotenv').config();
 
+const fs = require("fs");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ".";
 
 // array of all commands and embeds, normally that should be a .json somewhere but im lazy
-var arr = [];
+var embedArr = [];
+// array of command names and links to images, to be later stored in a .json file
+var linkArr = [];
+
+let rawData = fs.readFileSync('commands.json');
+if(rawData.length != 0)											// on first boot the .json is likely to be empty
+	linkArr = JSON.parse(rawData);
+
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+    for (var i = 0; i < linkArr.length; i++) {					// rebuild the embed array from the .json file 
+		var temp = new Discord.MessageEmbed();
+		temp.setImage(linkArr[i].link);
+		embedArr.push({ name: linkArr[i].name, embed: temp });
+	}
 });
 
 function isValidHttpUrl(str) {
@@ -24,9 +37,10 @@ function isValidHttpUrl(str) {
 }
 
 function doesExist(strName, strLink) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i].name == strName) {
-			arr[i].embed.setImage(strLink);
+    for (var i = 0; i < embedArr.length; i++) {
+        if (embedArr[i].name == strName) {
+			embedArr[i].embed.setImage(strLink);
+			linkArr[i].link = strLink;
 			return true;
         }
 	}
@@ -34,8 +48,8 @@ function doesExist(strName, strLink) {
 }
 
 function search(strName) {
-    for (var i = 0; i < arr.length; i++)
-        if (arr[i].name == strName) 
+    for (var i = 0; i < embedArr.length; i++)
+        if (embedArr[i].name == strName) 
 			return i;
 	return -1;
 }
@@ -57,21 +71,32 @@ client.on('message', message => {
 			if (args[1][0] == '<' && args[1][args[1].length - 1] == '>')
 				args[1] = args[1].slice(1, -1);
 
-			if (isValidHttpUrl(args[1]))
-				if (doesExist(args[0], args[1])) 
-					message.channel.send("Link replaced");
+		if (isValidHttpUrl(args[1])) {
+			if (doesExist(args[0], args[1]))
+				message.channel.send("Link replaced");
 
-				else {
-					var temp = new Discord.MessageEmbed();
-					temp.setImage(args[1]);
-					arr.push({ name: args[0], embed: temp });
+			else {
+				var temp = new Discord.MessageEmbed();
+				temp.setImage(args[1]);
+				embedArr.push({ name: args[0], embed: temp });
+				linkArr.push({ name: args[0], link: args[1] });
+			}
+			// write JSON string to a file
+			// yes i do rewrite the entire json upon adding a new command but who cares
+			fs.writeFileSync('commands.json', JSON.stringify(linkArr, null, 4), (err) => {		 // 2 last parameters in stringify make json look "pretty"
+				if (err) {
+					throw err; 
 				}
+				console.log("JSON data is saved.");
+			});
+		}
 			else
 				message.channel.send("Invalid link");
 
 	}
 	else if (search(command) != -1) 
-		message.channel.send(arr[search(command)].embed);
+		message.channel.send(embedArr[search(command)].embed);
+
 });
 
 
